73a364b6d8ace0a90e881635a30c728a40f7d32f
X.F. Zhou <mrchip.zxf@outlook.com>
2021-07-15 17:29:54 +0800

    FEAT: add some file access figure example in Ch. 4

---
+++ practice/F4.12/main.c
@@ -0,0 +1,23 @@
+#include <apue.h>
+#include <fcntl.h>
+#include <bits/stat.h>
+
+#define RWRWRW (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH)
+int main(int argc, char *argv[])
+{
+    umask(0);
+    if (creat("foo", RWRWRW) < 0)
+        err_sys("creat error for foo");
+    umask(S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    if (creat("bar", RWRWRW) < 0)
+        err_sys("creat error for bar");
+
+    struct stat stat_buff;
+    if (stat("foo", &stat_buff) < 0)
+        err_sys("stat error for foo");
+    if (chmod("foo", (stat_buff.st_mode & ~S_IXGRP) | S_ISGID) < 0)
+        err_sys("chmod error for foo");
+    if (chmod("bar", S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH) < 0)
+        err_sys("chmo error for bar");
+    exit(0);
+}
